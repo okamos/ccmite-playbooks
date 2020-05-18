9d3a3cc270b3c3a6f6aa8c7bf05c0f4e0832d85e
okamos <himinato.k@gmail.com>
2020-05-19 01:35:03 +0900

    Mirror the official twitter account

--- roles/discord/templates/main.py
+++ roles/discord/templates/main.py
@@ -1,7 +1,10 @@
-import discord
 import os
 import time
 import re
+import json
+import urllib.request
+import discord
+from discord.ext import tasks
 
 client = discord.Client()
 
@@ -17,7 +20,6 @@ async def on_message(message):
     if client.user in message.mentions:
         l = len(str(client.user.id)) + 5
         words = re.split('\s', message.content)
-        print(words)
         if len(words[1]) >= 3 and len(words[1]) <= 16:
             c = client.get_channel(618319969163280404)
             await c.send("新規ユーザー(" + words[1] + ")への対応をしてください http://ccmite.com:8084/ccadmin/user.php?name=" + words[1])
@@ -31,4 +33,40 @@ async def on_member_join(member):
     c = client.get_channel(608700399880503313)
     await c.send(member.name + "さん初めまして、 まずは <#611445694015995904>, <#611450466391556116> をご覧ください。その後に<@!659708908562022402>にプロフィール名を伝えてください")
 
+path = '/home/{{ ansible_user }}/.latest_tweet_id'
+url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+params = {
+    'screen_name': 'ccmite',
+    'count': 3,
+}
+@tasks.loop(seconds=60)
+async def loop():
+    latest = ''
+    texts = []
+    if os.path.isfile(path):
+        with open(path) as f:
+            latest = f.read()
+
+    req = urllib.request.Request('{}?{}'.format(url, urllib.parse.urlencode(params)))
+    req.add_header('Content-Type', 'application/json')
+    req.add_header('Authorization', 'Bearer {}'.format(os.environ.get('TWITTER_ACCESS_TOKEN')))
+    with urllib.request.urlopen(req) as res:
+        tweets = json.loads(res.read())
+        for t in tweets:
+            # run to latest tweet
+            if latest == str(t['id']):
+                break
+            if len(t['entities']['user_mentions']) == 0:
+                texts.append(t['text'])
+        latest = tweets[0]['id']
+
+    c = client.get_channel(711961465493651516)
+    if os.path.isfile(path):
+        for t in texts:
+            await c.send(t)
+
+    with open(path, mode='w') as f:
+        f.write(str(latest))
+
+loop.start()
 client.run(os.environ['DISCORD_BOT_API_TOKEN'])
